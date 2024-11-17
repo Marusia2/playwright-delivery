b736142ad2b0e09f4e3c798b06a458fcf448fd57
Marija <gestuverteja.marija@gmail.com>
2024-11-17 19:39:26 +0200

    16 assignment with implementing faker in test to generate random username and password

--- readme.md
+++ readme.md
@@ -11,3 +11,13 @@ signInButton - data-name="signIn-button"
 errorPopUpMessage - "authorizationError-popup"
 closeButton - "authorizationError-popup-close-button"
 
+**Random Data Generation**
+npm install @faker-js/faker
+
+**Environment**
+APP_URL='https://fe-delivery.tallinn-learning.ee/signin'
+
+**Run Application**
+npx playwright test
+or
+npx playwright test --headed
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,24 +1,31 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 
+// 16 assignment. Tests were rewritten using randomly generated usernames and passwords created by the Faker application.
+
+const baseURL = process.env.APP_URL || 'https://fe-delivery.tallinn-learning.ee/signin'
 test.beforeEach(async ({ page }) => {
-  await page.goto("https://fe-delivery.tallinn-learning.ee/signin");
-})
-test('Check for incorrect credentials message and close pop up message', async ({ page }) => {
-  const usernameField = page.getByTestId("username-input")
-  const passwordField = page.getByTestId("password-input")
-  const signInButton = page.getByTestId("signIn-button")
-  const errorPopUpMessage = page.getByTestId("authorizationError-popup")
-  const closeButton = page.getByTestId("authorizationError-popup-close-button")
-
-  await usernameField.fill("incorrectUsername")
-  await passwordField.fill("incorrectPassword")
-  await signInButton.click()
-  await expect(errorPopUpMessage).toBeVisible()
-  // await expect(signInButton).toBeDisabled()
-
-  await closeButton.click()
-  await expect(signInButton).toBeEnabled()
+  await page.goto(baseURL);
 });
+
+test('Check for incorrect credentials message and close pop up message', async ({ page }) => {
+  const usernameField = page.getByTestId("username-input");  // may use page.locator("test")
+  const passwordField = page.getByTestId("password-input");
+  const signInButton = page.getByTestId("signIn-button");
+  const errorPopUpMessage = page.getByTestId("authorizationError-popup");
+  const closeButton = page.getByTestId("authorizationError-popup-close-button");
+
+  await usernameField.fill(faker.internet.username());
+  await passwordField.fill(faker.internet.password());
+  await signInButton.click();
+  await expect(errorPopUpMessage).toBeVisible();
+
+  await closeButton.click();
+  await expect(signInButton).toBeEnabled();
+  console.log('Random username is: ', faker.internet.username())
+  console.log('Random password is: ', faker.internet.password())
+});
+
 
 test('Check for error messages for login input', async ({ page }) => {
   const usernameField = page.getByTestId("username-input")
